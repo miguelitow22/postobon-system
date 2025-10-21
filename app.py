b270b991bccfe36df6b9819b6f89f5e43d5a9e4b
Miguel Lopez <miguelopsal@gmail.com>
2025-10-21 00:28:38 -0500

    t

--- app.py
+++ app.py
@@ -51,7 +51,10 @@ def create_app():
 
     return app
 
+# ------------------------------
+# Creamos el objeto WSGI para Gunicorn
+# ------------------------------
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
